c31803ad0464e5489924760f492b6e46618d9f7d
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-05-11 09:23:20 +0200

    Create class and method to get userResponseDto object

---
+++ web/src/test/java/kazmierczak/jan/domain/model/user/dto/CreateUserResponseDtoTest.java
@@ -0,0 +1,43 @@
+package kazmierczak.jan.domain.model.user.dto;
+
+import model.user.User;
+import model.user.dto.CreateUserResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import types.Role;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateUserResponseDtoTest {
+
+    @Test
+    @DisplayName("when method returns correct object")
+    public void test1() {
+        var user = User
+                .builder()
+                .id(1L)
+                .name("name")
+                .surname("surname")
+                .age(18)
+                .email("email@email.pl")
+                .password("password")
+                .role(Role.USER)
+                .build();
+
+        var createUserResponseDto = CreateUserResponseDto
+                .builder()
+                .id(1L)
+                .name("name")
+                .surname("surname")
+                .age(18)
+                .email("email@email.pl")
+                .password("password")
+                .build();
+
+        assertThat(user.toCreateUserResponseDto())
+                .isEqualTo(createUserResponseDto);
+    }
+}
