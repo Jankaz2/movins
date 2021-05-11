be6aa4b2a664c5286b88f0fcfa59257d6d05f40d
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-05-11 10:19:43 +0200

    Create validator for creating user dto method

---
+++ domain/src/main/java/model/user/dto/validator/CreateUserDtoValidator.java
@@ -0,0 +1,86 @@
+package model.user.dto.validator;
+
+import config.validator.Validator;
+import model.user.dto.CreateUserDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateUserDtoValidator implements Validator<CreateUserDto> {
+    @Override
+    public Map<String, String> validate(CreateUserDto createUserDto) {
+        var errors = new HashMap<String, String>();
+
+        if(createUserDto == null) {
+            errors.put("create user dto", "is null");
+            return errors;
+        }
+
+        var name = createUserDto.getName();
+        if(hasIncorrectName(name)) {
+            errors.put("name", "is incorrect: " + name);
+        }
+
+        var surname = createUserDto.getSurname();
+        if(hasIncorrectSurname(surname)) {
+            errors.put("surname", "is incorrect: " + surname);
+        }
+
+        var age = createUserDto.getAge();
+        if(hasIncorrectAge(age)) {
+            errors.put("age", "is incorrect: " + age);
+        }
+
+        var email = createUserDto.getEmail();
+        if(hasIncorrectEmail(email)) {
+            errors.put("email", "is incorrect: " + email);
+        }
+
+        return errors;
+    }
+
+    /**
+     *
+     * @param name we want to validate
+     * @return true if name is null or is too short or syntax is wrong,
+     * otherwise return false
+     */
+    private boolean hasIncorrectName(String name) {
+        return name == null
+                || name.length() < 3
+                || !name.matches("^[A-Z][a-z]+$");
+    }
+
+    /**
+     *
+     * @param surname we want to validate
+     * @return true if surname is null or is too short or syntax is wrong,
+     * otherwise return false
+     */
+    private boolean hasIncorrectSurname(String surname) {
+        return surname == null
+                || surname.length() < 5
+                || !surname.matches("^[A-Z][a-z]+$");
+    }
+
+    /**
+     *
+     * @param age we want to valiate
+     * @return true if age is null or is less or equal to 0,
+     * otherwise return false
+     */
+    private boolean hasIncorrectAge(Integer age) {
+        return age == null || age <=0;
+    }
+
+    /**
+     *
+     * @param email we want to validate
+     * @return true if email is null or does not match syntax,
+     * otherwise return false
+     */
+    private boolean hasIncorrectEmail(String email) {
+        return email == null
+                || !email.matches("^[A-Za-z0-9.]{5,}@[a-z0-9]{2,}.[a-z]{2,}$");
+    }
+}
---
+++ web/src/test/java/kazmierczak/jan/domain/model/user/dto/validator/CreateUserDtoValidatorTest.java
@@ -0,0 +1,88 @@
+package kazmierczak.jan.domain.model.user.dto.validator;
+
+import config.validator.Validator;
+import config.validator.exception.ValidatorException;
+import model.user.dto.CreateUserDto;
+import model.user.dto.validator.CreateUserDtoValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateUserDtoValidatorTest {
+    @Test
+    @DisplayName("when object is null")
+    public void test1() {
+        var validator = new CreateUserDtoValidator();
+        assertThatThrownBy(() -> Validator.validate(validator, null))
+                .isInstanceOf(ValidatorException.class)
+                .hasMessage("[VALIDATION ERRORS] -> create user dto: is null");
+    }
+
+    @Test
+    @DisplayName("when name is wrong")
+    public void test2() {
+        var validator = new CreateUserDtoValidator();
+        var createUserDto = CreateUserDto.builder()
+                .name("a")
+                .surname("Jonas")
+                .age(10)
+                .email("john2001@wp.pl")
+                .build();
+        assertThatThrownBy(() -> Validator.validate(validator, createUserDto))
+                .isInstanceOf(ValidatorException.class)
+                .hasMessageStartingWith("[VALIDATION ERRORS] -> ")
+                .hasMessageContaining("name: is incorrect: a");
+    }
+
+    @Test
+    @DisplayName("when surname is wrong")
+    public void test3() {
+        var validator = new CreateUserDtoValidator();
+        var createUserDto = CreateUserDto.builder()
+                .name("Jonas")
+                .surname("a")
+                .age(10)
+                .email("john2001@wp.pl")
+                .build();
+        assertThatThrownBy(() -> Validator.validate(validator, createUserDto))
+                .isInstanceOf(ValidatorException.class)
+                .hasMessageStartingWith("[VALIDATION ERRORS] ->")
+                .hasMessageContaining("surname: is incorrect: a");
+    }
+
+    @Test
+    @DisplayName("when age is wrong")
+    public void test4() {
+        var validator = new CreateUserDtoValidator();
+        var createUserDto = CreateUserDto.builder()
+                .name("Jonas")
+                .surname("Jonas")
+                .age(0)
+                .email("john2001@wp.pl")
+                .build();
+        assertThatThrownBy(() -> Validator.validate(validator, createUserDto))
+                .isInstanceOf(ValidatorException.class)
+                .hasMessageStartingWith("[VALIDATION ERRORS] ->")
+                .hasMessageContaining("age: is incorrect: 0");
+    }
+
+    @Test
+    @DisplayName("when email is wrong")
+    public void test5() {
+        var validator = new CreateUserDtoValidator();
+        var createUserDto = CreateUserDto.builder()
+                .name("Jonas")
+                .surname("Jonas")
+                .age(10)
+                .email("john2001_@wp.pl")
+                .build();
+        assertThatThrownBy(() -> Validator.validate(validator, createUserDto))
+                .isInstanceOf(ValidatorException.class)
+                .hasMessageStartingWith("[VALIDATION ERRORS] ->")
+                .hasMessageContaining("email: is incorrect: john2001_@wp.pl");
+    }
+}
