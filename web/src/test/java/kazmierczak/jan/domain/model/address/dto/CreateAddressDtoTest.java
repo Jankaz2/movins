3e2af896754d4592ef043207fcb8aa7ccd8021db
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-06-05 21:30:39 +0200

    Create test for CreateAddressDto

---
+++ web/src/test/java/kazmierczak/jan/domain/model/address/dto/CreateAddressDtoTest.java
@@ -0,0 +1,33 @@
+package kazmierczak.jan.domain.model.address.dto;
+
+import model.address.Address;
+import model.address.dto.CreateAddressDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateAddressDtoTest {
+
+    @Test
+    @DisplayName("when method works correct")
+    public void test1 () {
+        var createAddressDto = CreateAddressDto
+                .builder()
+                .city("City")
+                .street("Street")
+                .number(12)
+                .build();
+
+        var address = Address
+                .builder()
+                .city("City")
+                .street("Street")
+                .number(12)
+                .build();
+        assertThat(address).isEqualTo(createAddressDto.toAddress());
+    }
+}
