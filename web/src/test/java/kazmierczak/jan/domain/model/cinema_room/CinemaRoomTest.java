55312a30a057e10e021374a5432ad83bfa99247b
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-06-05 21:50:20 +0200

    Create tests for CinemaRoom model classes

---
+++ web/src/test/java/kazmierczak/jan/domain/model/cinema_room/CinemaRoomTest.java
@@ -0,0 +1,53 @@
+package kazmierczak.jan.domain.model.cinema_room;
+
+import model.cinema_room.CinemaRoom;
+import model.cinema_room.dto.CreateCinemaRoomDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CinemaRoomTest {
+    @Test
+    @DisplayName("when toListOfCreateCinemaRoomDto method works correct")
+    public void test1() {
+        var cinemaRoomsList = List.of(
+          CinemaRoom.builder().name("Name1").rows(10).places(10).build(),
+          CinemaRoom.builder().name("Name2").rows(11).places(11).build()
+        );
+
+        var cinemaRoomsDtoList = List.of(
+          CreateCinemaRoomDto.builder().name("Name1").rows(10).places(10).build(),
+          CreateCinemaRoomDto.builder().name("Name2").rows(11).places(11).build()
+        );
+
+        assertThat(CinemaRoom.toListOfCreateCinemaRoomDto(cinemaRoomsList))
+                .isEqualTo(cinemaRoomsDtoList);
+    }
+
+    @Test
+    @DisplayName("when toCreateCinemaRoomDto method works correct")
+    public void test2() {   var cinemaRoom = CinemaRoom
+            .builder()
+            .name("Name")
+            .rows(10)
+            .places(10)
+            .build();
+
+        var createCinemaRoomDto = CreateCinemaRoomDto
+                .builder()
+                .name("Name")
+                .rows(10)
+                .places(10)
+                .build();
+
+
+        assertThat(cinemaRoom.toCreateCinemaRoomDto())
+                .isEqualTo(createCinemaRoomDto);
+    }
+}
---
+++ web/src/test/java/kazmierczak/jan/domain/model/cinema_room/dto/CreateCinemaRoomDtoTest.java
@@ -0,0 +1,54 @@
+package kazmierczak.jan.domain.model.cinema_room.dto;
+
+import model.cinema_room.CinemaRoom;
+import model.cinema_room.dto.CreateCinemaRoomDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateCinemaRoomDtoTest {
+
+    @Test
+    @DisplayName("when method toListOfCinemaRooms works correct")
+    public void test1() {
+        var cinemaRoomsDtoList = List.of(
+                CreateCinemaRoomDto.builder().name("Name1").rows(10).places(10).build(),
+                CreateCinemaRoomDto.builder().name("Name2").rows(11).places(11).build()
+        );
+
+        var cinemaRoomsList = List.of(
+                CinemaRoom.builder().name("Name1").rows(10).places(10).build(),
+                CinemaRoom.builder().name("Name2").rows(11).places(11).build()
+        );
+
+        assertThat(CreateCinemaRoomDto.toListOfCinemaRooms(cinemaRoomsDtoList))
+                .isEqualTo(cinemaRoomsList);
+    }
+
+    @Test
+    @DisplayName("when toCinemaRoom method works correct")
+    public void test2() {
+        var createCinemaRoomDto = CreateCinemaRoomDto
+                .builder()
+                .name("Name")
+                .rows(10)
+                .places(10)
+                .build();
+
+        var cinemaRoom = CinemaRoom
+                .builder()
+                .name("Name")
+                .rows(10)
+                .places(10)
+                .build();
+
+        assertThat(createCinemaRoomDto.toCinemaRoom())
+                .isEqualTo(cinemaRoom);
+    }
+}
