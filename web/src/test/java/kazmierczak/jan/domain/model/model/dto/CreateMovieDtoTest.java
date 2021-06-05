657dcb75f1f2826903085751d23efcf0b6f843af
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-06-05 21:59:09 +0200

    Create tests for Movie model classes

---
+++ web/src/test/java/kazmierczak/jan/domain/model/model/dto/CreateMovieDtoTest.java
@@ -0,0 +1,39 @@
+package kazmierczak.jan.domain.model.model.dto;
+
+import model.movie.Movie;
+import model.movie.dto.CreateMovieDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateMovieDtoTest {
+
+    @Test
+    @DisplayName("when method toMovie works correct")
+    public void test1() {
+        var createMovieDto = CreateMovieDto
+                .builder()
+                .title("Title")
+                .genre("Genre")
+                .duration(12)
+                .releaseDate(LocalDate.of(2021, 12, 12))
+                .build();
+
+        var movie = Movie
+                .builder()
+                .title("Title")
+                .genre("Genre")
+                .duration(12)
+                .releaseDate(LocalDate.of(2021, 12, 12))
+                .build();
+
+        assertThat(createMovieDto.toMovie())
+                .isEqualTo(movie);
+    }
+}
