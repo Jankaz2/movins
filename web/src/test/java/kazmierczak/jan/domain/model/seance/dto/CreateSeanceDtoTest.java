861402e8a27ebbbe50ce5c6c7476daccfcb7563f
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-08-12 23:38:31 +0200

    Fix CreateSeanceDto test

--- web/src/test/java/kazmierczak/jan/domain/model/seance/dto/CreateSeanceDtoTest.java
+++ web/src/test/java/kazmierczak/jan/domain/model/seance/dto/CreateSeanceDtoTest.java
@@ -1,6 +1,9 @@
 package kazmierczak.jan.domain.model.seance.dto;
 
-import kazmierczak.jan.model.movie.dto.CreateMovieDto;
+import kazmierczak.jan.model.address.Address;
+import kazmierczak.jan.model.cinema.Cinema;
+import kazmierczak.jan.model.cinema_room.CinemaRoom;
+import kazmierczak.jan.model.movie.Movie;
 import kazmierczak.jan.model.seance.Seance;
 import kazmierczak.jan.model.seance.dto.CreateSeanceDto;
 import org.junit.jupiter.api.DisplayName;
@@ -8,9 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
-
-import static org.assertj.core.api.Assertions.*;
+import static java.time.LocalDate.of;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 public class CreateSeanceDtoTest {
@@ -18,23 +20,14 @@ public class CreateSeanceDtoTest {
     @Test
     @DisplayName("when method toSeance works correct")
     public void test1() {
-        var creatreMovieDto = CreateMovieDto
-                .builder()
-                .title("Title")
-                .genre("Genre")
-                .duration(1)
-                .releaseDate(LocalDate.of(2021, 12, 12))
-                .build();
-
         var seance = Seance
                 .builder()
-                .movie(creatreMovieDto.toMovie())
-                .date(LocalDate.of(2021, 12, 12))
+                .date(of(2021, 12, 12))
                 .build();
 
         var createSeanceDto = CreateSeanceDto
                 .builder()
-                .date(LocalDate.of(2021, 12, 12))
+                .date(of(2021, 12, 12))
                 .build();
 
         assertThat(createSeanceDto.toSeance())
